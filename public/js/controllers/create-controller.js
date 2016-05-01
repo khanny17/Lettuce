fe0ff28a0442a5456174f104cdc04a7dc02ecd0d
Kevin <kph1958@rit.edu>
2016-04-30 19:28:38 -0700

    Adding validation for team name checkbox to test if the name is taken

--- public/js/controllers/create-controller.js
+++ public/js/controllers/create-controller.js
@@ -1,8 +1,23 @@
 'use strict';
 angular.module('CreateController', [])
 
-.controller('createController', ['$scope', function($scope){
+.controller('createController', ['$scope', 'teamService',
+    function($scope, teamService){
     $scope.team = {};
     $scope.validationErrors = {};
     $scope.validationSuccess = {};
+
+    $scope.$watch('team.name', function(newVal, oldVal){
+        if(newVal === oldVal){
+            return;
+        }
+
+        teamService.getTeamNames(function(teamNames){
+            if(teamNames.indexOf(newVal) >= 0){
+                $scope.validationErrors.name = 'That team already has a page';
+            } else {
+                $scope.validationErrors.name = null;
+            }
+        });
+    });
 }]);
