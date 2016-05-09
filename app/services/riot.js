4fcad645d74b1beced0c48d3e9fdd6f876787444
Kevin <kph1958@rit.edu>
2016-05-09 18:43:30 -0400

    removing unused functions in update

--- app/services/riot.js
+++ app/services/riot.js
@@ -8,7 +8,6 @@ var lodash = require('lodash');
 var apiCall = require('../utilities/apiHandler.js');
 
 //All the models we will be updating
-var TeamMatch = require('../models/teamMatch');
 var Summoner = require('../models/summoner');
 var Champion = require('../models/champion');
 var Version = require('../models/version');
@@ -37,11 +36,9 @@ var init = function(updateRate){
 var RunUpdate = function(){
     logger.info('Riot update job is running');
     var promises = [
-        // update.summoners(config.riot.ourTeam),
-        // update.teamMatches(config.riot.teamId, config.riot.ourTeamName),
-        // update.champions(),
-        // update.champMasteries(),
-        // update.winRateFinder(),
+        update.champions(),
+        update.champMasteries(),
+        update.winRateFinder(),
         update.statFinder()
     ];
 
@@ -116,100 +113,6 @@ var update = {
         return deferred.promise;
     },
 
-    //Update our summoner data
-    summoners: function(names){
-        var deferred = q.defer();
-        var error;
-        if(!names){
-            error = 'No summoner names provided, not updating any!';
-            logger.warn(error);
-            deferred.reject(error);
-            return deferred.promise;
-        }
-        if(!names.length){
-            error = 'Names is not an array!';
-            logger.error(error);
-            deferred.reject(error);
-            return deferred.promise;
-        }
-        //convert array to csv list
-        names = names.join(',');
-        
-        var base = config.riot.endpointUrls.summoner;
-        var url = base + names;
-
-        apiCall(url)
-        .then(function(summonerData){
-            //Convert json object with key-value to an array of just values
-            var values = lodash.values(summonerData);
-            var promises = [];
-            lodash.forEach(values, function(summoner){
-                promises.push(Summoner.create(summoner.id, summoner.name));
-            });
-            return q.all(promises);
-        })
-        .then(deferred.resolve)
-        .fail(deferred.reject);
-        return deferred.promise;
-    },
-
-
-    //Gets a summary of the team's matches and stored in db
-    teamMatches: function(teamId, teamName){
-        var deferred = q.defer();
-        if(!teamId){
-            var error = 'No team ID provided, i\'m not updating the team matches';
-            logger.warn(error);
-            deferred.reject(error);
-            return deferred.promise;
-        }
-
-        var url = config.riot.endpointUrls.team + teamId;
-
-        apiCall(url)
-        .then(function(teamData){
-            var matches = teamData[teamId].matchHistory;
-            var promises = [];
-            lodash.forEach(matches, function(match){
-                //Now modify the match data to work with our schema
-                var modelData = match;
-                modelData.date = new Date(modelData.date); //change epoch millis to Date
-                modelData.id = modelData.gameId; //make the "id" the game id
-                modelData.teamId = teamId;
-                delete modelData.gameId;
-                //Create the summary
-                promises.push(TeamMatch.create(modelData));              
-            });
-            return q.allSettled(promises); //wait for all to finish
-        })
-        .then(function(results){
-            var promises = [];
-            lodash.forEach(results, function(result){
-                //only create new details for matches we didn't have
-                if(result.state === 'fulfilled'){
-                    var modelData = result.value;
-                    //if we succeeded:, get the DETAILED match info:
-                    if(!modelData){
-                        logger.warn('Null match, not updating ');
-                    } else if(modelData === config.errors.alreadyExists){
-                        logger.debug('Match exists, not updating');
-                    } else if(modelData.win){ //figure out which team won/lost
-                        promises.push(
-                            update.match(
-                                modelData.id, teamName, modelData.opposingTeamName));
-                    } else {
-                        promises.push(
-                            update.match(
-                                modelData.id, modelData.opposingTeamName, teamName));
-                    }
-                }
-            });
-            return q.all(promises);
-        })
-        .then(deferred.resolve)
-        .fail(deferred.reject);
-        return deferred.promise;
-    },
     champMasteries: function(summonerName){
         var deferred = q.defer();
         //if they give a name, get just that one's masteries.
