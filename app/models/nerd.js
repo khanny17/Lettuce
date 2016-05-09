bc9752a81514c3bc960dcc61370bdafc3832bcc9
Kevin <kph1958@rit.edu>
2016-05-09 13:18:15 -0400

    removing useless file

--- app/models/nerd.js
+++
@@ -1,8 +0,0 @@
-// app/models/nerd.js
-var mongoose = require('mongoose');
-
-// define our nerd model
-// module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Nerd', {
-    name : {type : String, default: ''}
-});
